a16d4d7c0a70bd2f7d978aa6f143672916c067f4
Tim Engel <engeltj@gmail.com>
2015-06-10 16:04:25 -0400

    Update countries_controller.rb

--- api/app/controllers/spree/api/countries_controller.rb
+++ api/app/controllers/spree/api/countries_controller.rb
@@ -6,8 +6,7 @@ module Spree
 
       def index
         @countries = Spree::Country.accessible_by(current_ability, :read).ransack(params[:q]).result.
-                     includes(:states).order('name ASC').
-                     page(params[:page]).per(params[:per_page])
+                     includes(:states).order('name ASC')
         country = Spree::Country.order("updated_at ASC").last
         if stale?(country)
           respond_with(@countries)
